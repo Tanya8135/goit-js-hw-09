c021883b20f852a17b94b37ed10b304b4053e551
tanya8135 <108739694+Tanya8135@users.noreply.github.com>
2023-04-12 13:18:54 +0300

    done first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
-// перемикач кольорів
+// color switcher
+const btnStart = document.querySelector("[data-start]");
+const btnStop = document.querySelector("[data-stop]");
+let timerBtnStart;
+
+btnStart.addEventListener('click', () => {
+// вимкнення кнопки (робимо неауктивну використовуючи disabled)
+btnStart.disabled = true;
+// запуск таймера
+timerBtnStart = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor(); /* інлайновий стиль */
+}, 1000);
+});
+
+btnStop.addEventListener('click', () => {
+    btnStart.disabled = false; /* включення кнопки "Start" */
+    clearInterval(timerBtnStart); /* зупинення таймера */
+})
+
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
