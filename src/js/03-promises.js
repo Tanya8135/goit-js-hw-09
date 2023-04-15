7e1b16ca07dcc5fa1ed5e7916c94b6f78c278d68
tanya8135 <108739694+Tanya8135@users.noreply.github.com>
2023-04-16 01:14:26 +0300

    done 3 task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,47 @@
+// import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('.form');
+const delayInput = document.querySelector('input[name="delay"]');
+const stepInput = document.querySelector('input[name="step"]');
+const amountInput = document.querySelector('input[name="amount"]');
+const submitBtn = document.querySelector('button[type="submit"]');
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        // Fulfill
+        Notiflix.Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
+        resolve({ position, delay });
+      } else {
+        // Reject
+        Notiflix.Notify.failure(`Rejected promise ${position} in ${delay}ms`);
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
+
+submitBtn.addEventListener('click', (evt) => {
+  evt.preventDefault();
+
+  /* буде отримуватися значення з введених полів форми */
+const delayValue = parseInt(delayInput.value);
+const stepValue = parseInt(stepInput.value);
+const amountValue = parseInt(amountInput.value);
+
+for (let i = 0; i < amountValue; i += 1) {
+  const position = i + 1;
+  const delay = delayValue + i * stepValue;
+  createPromise(position, delay)
+    .then(() => {
+
+      Notiflix.Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(() => {
+      Notiflix.Notify.failure(`Rejected promise ${position} in ${delay}ms`);
+    })
+};
+});
